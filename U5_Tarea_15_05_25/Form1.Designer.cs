caa912763af4476321a22ca9d86a4e5d8b8e32bf
carolina sustaita de luna <CaroSus3102@outlook.es>
2025-05-15 22:55:40 -0600

    prueba

--- U5_Tarea_15_05_25/Form1.Designer.cs
+++ U5_Tarea_15_05_25/Form1.Designer.cs
@@ -86,9 +86,9 @@
             this.label1.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(547, 43);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(180, 23);
+            this.label1.Size = new System.Drawing.Size(187, 23);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Archivo Seleccionado";
+            this.label1.Text = "Archivo Seleccionados";
             // 
             // Form1
             // 
