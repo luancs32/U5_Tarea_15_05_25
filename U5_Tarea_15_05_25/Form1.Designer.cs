69fc16a1b70f7d27d42c454e4071322bc2450054
Carolina <Carolina@MSI>
2025-05-15 22:51:21 -0600

    Agregar archivos de proyecto.

---
+++ U5_Tarea_15_05_25/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace U5_Tarea_15_05_25
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBusqueda = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtSeleccion = new System.Windows.Forms.TextBox();
+            this.lstResultados = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBusqueda
+            // 
+            this.btnBusqueda.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnBusqueda.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBusqueda.Location = new System.Drawing.Point(313, 21);
+            this.btnBusqueda.Name = "btnBusqueda";
+            this.btnBusqueda.Size = new System.Drawing.Size(102, 38);
+            this.btnBusqueda.TabIndex = 0;
+            this.btnBusqueda.Text = "Buscar";
+            this.btnBusqueda.UseVisualStyleBackColor = false;
+            this.btnBusqueda.Click += new System.EventHandler(this.btnBusqueda_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.Color.Azure;
+            this.txtNombre.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(56, 19);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(206, 47);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtSeleccion
+            // 
+            this.txtSeleccion.BackColor = System.Drawing.Color.Azure;
+            this.txtSeleccion.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSeleccion.Location = new System.Drawing.Point(504, 93);
+            this.txtSeleccion.Multiline = true;
+            this.txtSeleccion.Name = "txtSeleccion";
+            this.txtSeleccion.Size = new System.Drawing.Size(262, 314);
+            this.txtSeleccion.TabIndex = 3;
+            // 
+            // lstResultados
+            // 
+            this.lstResultados.BackColor = System.Drawing.Color.Azure;
+            this.lstResultados.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstResultados.HideSelection = false;
+            this.lstResultados.Location = new System.Drawing.Point(21, 93);
+            this.lstResultados.Name = "lstResultados";
+            this.lstResultados.Size = new System.Drawing.Size(436, 314);
+            this.lstResultados.TabIndex = 4;
+            this.lstResultados.UseCompatibleStateImageBehavior = false;
+            this.lstResultados.DoubleClick += new System.EventHandler(this.lstResultados_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label1.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(547, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Archivo Seleccionado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstResultados);
+            this.Controls.Add(this.txtSeleccion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnBusqueda);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBusqueda;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtSeleccion;
+        private System.Windows.Forms.ListView lstResultados;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ U5_Tarea_15_05_25/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U5_Tarea_15_05_25
+{
+    
+    public partial class Form1 : Form
+    {
+        string NombreArchivo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ////Crea un programa que pida al usuario el nombre
+        ///de un fichero de texto y busque los archivos que
+        ///inicien con lo que tecleo el usuario, muestre la
+        ///información en una lista de todos los archivos que 
+        ///se llamen similar lo selecciones y te muestre el contenido
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstResultados.View = View.List;
+            lstResultados.LabelEdit = true;
+            lstResultados.GridLines = true;
+            lstResultados.FullRowSelect = true;
+            lstResultados.Columns.Add("Nombre", -2, HorizontalAlignment.Left);
+        }
+        //HOLA 
+        private void btnBusqueda_Click(object sender, EventArgs e)
+        {
+            string nombreFichero = txtNombre.Text.Trim();
+            lstResultados.Items.Clear();
+
+            string ruta = Directory.GetCurrentDirectory();
+            DirectoryInfo dir = new DirectoryInfo(ruta);
+            var archivos = dir.GetFiles("*.txt")
+                .Where(f => f.Name.StartsWith(nombreFichero, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            foreach (var fil in archivos)
+            {
+                lstResultados.Items.Add(fil.Name);
+            }
+
+            if (lstResultados.Items.Count == 0)
+            {
+                MessageBox.Show("No se encontraron archivos que coincidan con la búsqueda.");
+            }
+            else
+            {
+                MessageBox.Show("Se encontraron " + lstResultados.Items.Count + " archivos que coinciden con la búsqueda.");
+            }
+        }
+
+        private void lstResultados_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstResultados.SelectedItems.Count > 0)
+            {
+                NombreArchivo = lstResultados.SelectedItems[0].Text;
+                txtSeleccion.Clear();
+
+                string ruta = Path.Combine(Directory.GetCurrentDirectory(), NombreArchivo);
+                using (StreamReader archivo = new StreamReader(ruta))
+                {
+                    while (!archivo.EndOfStream)
+                    {
+                        txtSeleccion.AppendText(archivo.ReadLine() + Environment.NewLine);
+                    
+                    }
+                }
+            }
+            
+        }
+    }
+}
